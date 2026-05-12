064f0ba6bc182b35737b3ad14fd63de4356b1d14
WilliamMontes-11 <montesacostawilliamandres@gmail.com>
2026-05-11 19:05:32 -0500

    Compromiso Inicial - Software Fj.py
    Se añadieron comentarios para explicar el funcionamiento de las excepciones personalizadas utilizadas en el sistema.

--- excepciones.py
+++ excepciones.py
@@ -1,6 +1,8 @@
+# Esta excepción se llega a usar cuando un servicio no llega a estar disponible
 class ServicioNoDisponible(Exception):
     pass
 
 
+# Esta excepción aparece cuando una reserva tiene datos los cuales son incorrectos
 class ReservaInvalida(Exception):
     pass
