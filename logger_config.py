9b98ed20ec96f8ed3fa2c58e36f8d19d9772c379
WilliamMontes-11 <montesacostawilliamandres@gmail.com>
2026-05-11 19:09:40 -0500

    Compromiso Inicial - Software Fj
    Se añadieron comentarios explicativos en cuanto a la configuración del sistema de registro de errores mediante el logging.

--- logger_config.py
+++ logger_config.py
@@ -1,7 +1,11 @@
+# Importamos esta librería de logging para registrar diversos errores del sistema
 import logging
 
+# Configuración del archivo en el cual se guardarán los errores
 logging.basicConfig(
     filename="logs.txt",
+    # Solo se registrarán errores de tipo ERROR o superiores
     level=logging.ERROR,
+    # Formato que tendrán los mensajes guardados en el log
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
